77091edd9baa1e107aca3aa418afdd76cbcabe51
Dilusha <chakrawarthigedilushaeshan@gmail.com>
2026-04-17 17:07:59 +0530

    Home and AddJob Controller

---
+++ src/main/java/com/dilusha/jobapp/JobController.java
@@ -0,0 +1,23 @@
+package com.dilusha.jobapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JobController {
+
+    @RequestMapping({"/", "home"})
+    public String home(){
+        return "home";
+    }
+
+    @RequestMapping("addjob")
+    public String addJob(){
+        return "addjob";
+    }
+
+    @RequestMapping("viewalljobs")
+    public String viewAllJobs(){
+        return "viewalljobs";
+    }
+}
