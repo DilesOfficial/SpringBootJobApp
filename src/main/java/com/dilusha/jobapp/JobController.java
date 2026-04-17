6bcb1159e6c252647036f4ee7bb7435ab1b64ba4
Dilusha <chakrawarthigedilushaeshan@gmail.com>
2026-04-17 17:44:46 +0530

    View Data

--- src/main/java/com/dilusha/jobapp/JobController.java
+++ src/main/java/com/dilusha/jobapp/JobController.java
@@ -4,9 +4,12 @@ import com.dilusha.jobapp.model.JobPost;
 import com.dilusha.jobapp.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class JobController {
@@ -25,7 +28,9 @@ public class JobController {
     }
 
     @RequestMapping("viewalljobs")
-    public String viewAllJobs(){
+    public String viewAllJobs(Model model){
+        List<JobPost> jobs = jobService.getAllJobs();
+        model.addAttribute("jobPosts", jobs);
         return "viewalljobs";
     }
 
